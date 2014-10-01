fef3394ba9a99a9afe0163a14a6dc82af8107104
opedrocecilio <pedro.mc21@gmail.com>
2014-10-01 15:54:02 +0100

    Sapo desenhado
    os z's ainda nao estao certos, mas isso faz-se quando a camara 3D
    estiver a dar

--- ConsoleApplication2/Frog.cpp
+++ ConsoleApplication2/Frog.cpp
@@ -10,18 +10,105 @@ Frog::Frog(double x, double y, double z){
 Frog::~Frog(){}
 void Frog::draw(){
 	//printf("%d\n", getSpeed()->getY());
+	/*Cubo da cabeça*/
 		glPushMatrix();
+			glTranslated(0, -1, 1);
 			glColor3f(0, 1, 0);
-			glutSolidCube(4);
+			glScalef(1, 0.8, 1);
+			glutSolidCube(5);
 			glColor3f(0, 0.9, 0);
-			glutWireCube(4);
+			glutWireCube(5);
 		glPopMatrix();
+		/*Cubo do corpo*/
 		glPushMatrix();
-			glTranslated(0, -4, 1);
+			glTranslated(0, -5, 1);
 			glColor3f(0, 1, 0);
-			glutSolidCube(4);
+			glScalef(1, 0.8, 1);
+			glutSolidCube(5);
 			glColor3f(0, 0.9, 0);
-			glutWireCube(4);
-			glPopMatrix();
+			glutWireCube(5);
 		glPopMatrix();
+		/*Cubo de parte da pata superior direita que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(3, -1, 0);
+			glColor3f(0, 1, 0);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo do resto da pata superior direita que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(4, 0, 1);
+			glColor3f(0, 1, 0);
+			glScalef(1, 3, 1);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo de parte da pata superior esquerda que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(-3, -1, 0);
+			glColor3f(0, 1, 0);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo do resto da pata superior esquerda que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(-4, 0, 1);
+			glColor3f(0, 1, 0);
+			glScalef(1, 3, 1);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo de parte da pata inferior direita que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(3, -5, 0);
+			glColor3f(0, 1, 0);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo do resto da pata inferior direita que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(4, -6.5, 1);
+			glColor3f(0, 1, 0);
+			glScalef(1, 4, 1);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo de parte da pata inferior esquerda que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(-3, -5, 0);
+			glColor3f(0, 1, 0);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+		/*Cubo do resto da pata inferior esquerda que está encostada ao corpo*/
+		glPushMatrix();
+			glTranslated(-4, -6.5, 1);
+			glColor3f(0, 1, 0);
+			glScalef(1, 4, 1);
+			glutSolidCube(1);
+			glColor3f(0, 0.9, 0);
+			glutWireCube(1);
+		glPopMatrix();
+
+		/*Olho esquerdo do sapo*/
+		glPushMatrix();
+			glTranslated(-2, 1, 0);
+			glColor3f(1, 0, 0);
+			glutSolidSphere(1, 16, 16);
+		glPopMatrix();
+
+		/*Olho direito do sapo*/
+		glPushMatrix();
+			glTranslated(2, 1, 0);
+			glColor3f(1, 0, 0);
+			glutSolidSphere(1, 16, 16);
+		glPopMatrix();
+
 }
