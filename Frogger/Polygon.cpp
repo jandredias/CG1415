123ba9f0b81d71ae5067cce0a26e25fbd62881cf
pcdiasmega <jdias.email@gmail.com>
2014-11-04 14:40:20 +0000

    ppoligono2

--- Frogger/Polygon.cpp
+++ Frogger/Polygon.cpp
@@ -12,37 +12,47 @@ void Polygon::execute(GLfloat w, GLfloat h, GLfloat qt){
 	
 	GLfloat step_w = w / w_nr;
 	GLfloat step_h = h / h_nr;
-	
+
+	glPushMatrix();
+	glTranslatef(-w / 2, -h / 2, 0);
 	for(GLfloat x = 0; x <= w; x += step_w){
 		for(GLfloat y = 0; y <= h; y += step_h){
 			glPushMatrix();
-				glColor3f
-			
-			glPopMatrix();
-		}
-	}
-	GLfloat qt_w = qt;
-	GLfloat qt_h = w / qt;
-	GLfloat width = w / qt_w;
-	GLfloat height = h / qt_h;
-	glPushMatrix();
-	glTranslatef(-w / 2, -h / 2, 0);
-	
-	for (GLfloat x = 0; x < qt_w; x++){
-		for (GLfloat y = 0; y < qt_h; y++){
-			glPushMatrix();
-				glColor3f(rand() % 100 * 0.01,rand() % 100 * 0.01,rand() % 100 * 0.01);
-				glTranslatef(x * width, y*height, 0);
-				glBegin(GL_POLYGON);
+			glColor3f(rand() % 100 * 0.01, rand() % 100 * 0.01, rand() % 100 * 0.01);
+			glTranslatef(x, y, 0);
+			glBegin(GL_POLYGON);
 					glNormal3f(0, 0, 1);
 					glVertex3f(0, 0, 0);
-					glVertex3f(width, 0, 0);
-					glVertex3f(width, height, 0);
-					glVertex3f(0, height, 0);
+					glVertex3f(step_w, 0, 0);
+					glVertex3f(step_w, step_h, 0);
+					glVertex3f(0, step_h, 0);
 				glEnd();
 			glPopMatrix();
 		}
 	}
-	
 	glPopMatrix();
+	//GLfloat qt_w = qt;
+	//GLfloat qt_h = w / qt;
+	//GLfloat width = w / qt_w;
+	//GLfloat height = h / qt_h;
+	//glPushMatrix();
+	//glTranslatef(-w / 2, -h / 2, 0);
+	//
+	//for (GLfloat x = 0; x < qt_w; x++){
+	//	for (GLfloat y = 0; y < qt_h; y++){
+	//		glPushMatrix();
+	//			glColor3f(rand() % 100 * 0.01,rand() % 100 * 0.01,rand() % 100 * 0.01);
+	//			glTranslatef(x * width, y*height, 0);
+	//			glBegin(GL_POLYGON);
+	//				glNormal3f(0, 0, 1);
+	//				glVertex3f(0, 0, 0);
+	//				glVertex3f(width, 0, 0);
+	//				glVertex3f(width, height, 0);
+	//				glVertex3f(0, height, 0);
+	//			glEnd();
+	//		glPopMatrix();
+	//	}
+	//}
+	//
+	//glPopMatrix();
 }
