093e100444029b31bc7525d26e4b1af3679af400
pcdiasmega <jdias.email@gmail.com>
2014-11-21 13:38:54 +0000

    textura asphalts

--- Frogger/Road.cpp
+++ Frogger/Road.cpp
@@ -15,8 +15,8 @@ void Road::draw(){
 						getPosition().getY(),
 						0);
 		defineMaterial(	0.00, 0.00, 0.00, 1.00,	//Ambient
-						1.00, 1.00, 1.00, 1.00,	//Diffuse
-						1.00, 1.00, 1.00, 1.00,	//Specular
+						0.20, 0.20, 0.20, 1.00,	//Diffuse
+						0.20, 0.20, 0.20, 1.00,	//Specular
 						0.00, 0.00, 0.00, 1.00,	//Emission
 						77, getTexture());					//SHININESS
 		glColor3f(0.1137, 0.1137, 0.1137);
